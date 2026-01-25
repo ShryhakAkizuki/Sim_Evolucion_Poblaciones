f79bee3c4138a137aca32cb8a3b68fe7d24d5add
ShryhakAkizuki <oriverosp@unal.edu.co>
2026-01-25 17:33:37 -0500

    Backup

--- tests/data_structures/test_Node_Templates.cpp
+++ tests/data_structures/test_Node_Templates.cpp
@@ -26,10 +26,99 @@ TEST(NodeBaseTest, ConstructorWithForward) {
     EXPECT_EQ(node.Value(), 2);
 }
 
+// ----- Acceso de elementos -----
 
+TEST(NodeBaseTest, GetValue) {
+    NodeBase<int> node(1000);
+    EXPECT_EQ(node.Value(), 1000);
+}
 
+// ----- Modificacion -----
+
+TEST(NodeBaseTest, SetValueConst) {
+    NodeBase<int> node(1000);
+    EXPECT_EQ(node.Value(), 1000);
+
+    int x = 2000;
+    node.SetValue(x);
+    EXPECT_EQ(node.Value(), 2000);
+}
+
+TEST(NodeBaseTest, SetValueMove) {
+    NodeBase<int> node(1000);
+    EXPECT_EQ(node.Value(), 1000);
+
+    node.SetValue(2000);
+    EXPECT_EQ(node.Value(), 2000);
+}
+
+TEST(NodeBaseTest, SetValueForward) {
+    NodeBase<int> node(1000);
+    EXPECT_EQ(node.Value(), 1000);
+
+    node.SetValue(int(2000));
+    EXPECT_EQ(node.Value(), 2000);
+}
+
+// ----- Comparadores -----
+
+TEST(NodeBaseTest, Operators_EQ_NE) {
+    NodeBase<int> node(1000);
+    NodeBase<int> node1(1000);
+    NodeBase<int> node2(100);
+
+    EXPECT_TRUE(node==node1);
+    EXPECT_FALSE(node==node2);
+    EXPECT_FALSE(node2==node1);
+
+    EXPECT_FALSE(node!=node1);
+    EXPECT_TRUE(node!=node2);
+    EXPECT_TRUE(node2!=node1);
+}
 
 // #################### DListNode - Class ###################
+
+TEST(DListNodeTest, DefaultConstructor) {
+    DListNode<int> node;
+    EXPECT_EQ(node.Value(), int());
+    EXPECT_EQ(node.Next(), nullptr);
+    EXPECT_EQ(node.Back(), nullptr);
+}
+
+TEST(DListNodeTest, ConstructorWithValue) {
+    int xd = 100;
+    DListNode<int> node(xd);
+    EXPECT_EQ(node.Value(), 100);
+    EXPECT_EQ(node.Next(), nullptr);
+    EXPECT_EQ(node.Back(), nullptr);
+}
+
+TEST(DListNodeTest, ConstructorWithValue_Pointers_1) {
+    int xd = 100;
+    DListNode<int> node(xd);
+    std::unique_ptr<DListNode<int>> xD = std::make_unique<
+    EXPECT_EQ(node.Value(), 100);
+    EXPECT_EQ(node.Next(), nullptr);
+    EXPECT_EQ(node.Back(), nullptr);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
